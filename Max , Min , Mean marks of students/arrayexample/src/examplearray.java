2cf83803b49bef98ce0c6cdb026c18d203f67f62
Shyam Sundar Velmurugan <145095417+HwuShyam2005@users.noreply.github.com>
2024-06-22 20:11:40 +0400

    Max, Min, Mean marks code uploaded

---
+++ Max , Min , Mean marks of students/arrayexample/src/examplearray.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class examplearray {
+
+	public static void main(String[] args) {
+	double[] scores; // lab scores
+    int students; // number of students
+    double lowest; // lowest score
+	double highest; // highest score
+	double mean; // mean score
+			
+	// first, find out how many students there are
+	System.out.println("How many students?");
+	Scanner scan = new Scanner(System.in);
+	students = scan.nextInt();
+	
+	// make the scores array the correct size
+	scores = new double[students];
+	
+	// read scores from user
+	System.out.println("Please input scores:");
+	for(int i=0; i<scores.length; i++)
+	scores[i] = scan.nextDouble();
+	
+	// find lowest score
+	lowest = Double.POSITIVE_INFINITY;
+	for(double score : scores)
+	if(score<lowest)
+	lowest = score;
+	
+	// find highest score
+	highest = Double.NEGATIVE_INFINITY;
+	for(double score : scores)
+	if(score>highest)
+	highest = score;
+	
+	// find mean score
+	mean = 0;
+	for(double score : scores)
+	mean += score;
+	mean /= students;
+	
+	// output info
+	System.out.println("The scores were between"
+	+ lowest + " and " + highest
+	+ " with a mean of " + mean);
+
+	}
+
+}
